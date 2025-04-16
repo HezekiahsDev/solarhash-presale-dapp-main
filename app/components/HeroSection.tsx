111da5ecd9965f32be900da86359c1aaaa22cf14
heze <heze>
2025-04-16 22:54:51 +0100

    modify UX

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,15 +1,8 @@
-"use client";
-import { useEffect } from "react";
-import AOS from "aos";
 import "aos/dist/aos.css";
 import Welcome from "./Welcome";
 import BuyTokens from "./BuyTokens";
 
 const HeroSection = () => {
-  useEffect(() => {
-    AOS.init({ duration: 1000 });
-  }, []);
-
   return (
     <section
       className="pt-32 pb-12 flex flex-col lg:flex-row items-center justify-center space-y-8 lg:space-y-0 lg:space-x-8 px-4 bg-cover bg-center border-b-4 border-yellow-500"
